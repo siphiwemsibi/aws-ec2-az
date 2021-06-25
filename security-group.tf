f91d2a68b9b34af1e43320b4dd872ee58891bb28
Siphiwe <Siphiwe.Msibi@standardbank.co.za>
2021-06-25 14:26:48 +0200

    sec and main mod1

--- security-group.tf
+++ security-group.tf
@@ -13,13 +13,13 @@ resource "aws_security_group" "firco_sec" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   description = "Allow https"
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    description = "Allow https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     description = "Egress traffic"
